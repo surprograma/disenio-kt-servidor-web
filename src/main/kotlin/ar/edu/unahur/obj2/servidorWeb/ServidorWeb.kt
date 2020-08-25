de0e0a9f5301df166c9b7ade0f087272a7351aab
Federico Aloi <federico.aloi@gmail.com>
2020-08-25 20:21:30 -0300

    Devuelve 200 si el protocolo es HTTP

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
@@ -2,9 +2,17 @@ package ar.edu.unahur.obj2.servidorWeb
 
 import java.time.LocalDateTime
 
-class ServidorWeb {
-  fun realizarPedido(ip: String, url: String, fechaHora: LocalDateTime) =
-    Respuesta(codigo = 501, body = "")
+enum class CodigoHttp(val codigo: Int) {
+  OK(200),
+  NOT_IMPLEMENTED(501),
 }
 
-class Respuesta(val codigo: Int, val body: String)
+class ServidorWeb {
+  fun realizarPedido(ip: String, url: String, fechaHora: LocalDateTime) =
+    if (url.startsWith("http:"))
+      Respuesta(codigo = CodigoHttp.OK, body = "todo bien")
+    else
+      Respuesta(codigo = CodigoHttp.NOT_IMPLEMENTED, body = "")
+}
+
+class Respuesta(val codigo: CodigoHttp, val body: String)
--- src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
@@ -1,22 +1,23 @@
 package ar.edu.unahur.obj2.servidorWeb
 
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
-import java.lang.ArithmeticException
 import java.time.LocalDateTime
 
 class ServidorWebTest : DescribeSpec({
-//  Los servidores que modelemos van a aceptar solamente el protocolo HTTP.
-//  Si el protocolo de la URL es distinto a "http" hay que devolver código de respuesta 501
-//  (servicio no implementado) y body vacío.
-
   describe("Un servidor web") {
+    val servidor = ServidorWeb()
+
     it("devuelve 501 si recibe un pedido que no es HTTP") {
-      val servidor = ServidorWeb()
       val respuesta = servidor.realizarPedido("207.46.13.5", "https://pepito.com.ar/hola.txt", LocalDateTime.now())
-      respuesta.codigo.shouldBe(501)
+      respuesta.codigo.shouldBe(CodigoHttp.NOT_IMPLEMENTED)
       respuesta.body.shouldBe("")
+    }
+
+    it("devuelve 200 si recibe un pedido HTTP") {
+      val respuesta = servidor.realizarPedido("207.46.13.5", "http://pepito.com.ar/hola.txt", LocalDateTime.now())
+      respuesta.codigo.shouldBe(CodigoHttp.OK)
+      respuesta.body.shouldBe("todo bien")
     }
   }
 })
