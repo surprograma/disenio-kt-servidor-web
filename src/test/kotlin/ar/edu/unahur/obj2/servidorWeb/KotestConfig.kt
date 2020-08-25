9e6793439df4409c47de547da6809c09900fa1da
Federico Aloi <federico.aloi@gmail.com>
2020-08-25 19:10:12 -0300

    Modificado IsolationMode de kotest

---
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/KotestConfig.kt
@@ -0,0 +1,10 @@
+package ar.edu.unahur.obj2.servidorWeb
+
+import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.core.spec.IsolationMode
+
+class KotestConfig: AbstractProjectConfig() {
+  // Modificamos esta configuración para que se creen nuevos objetos para cada `it`.
+  override val isolationMode = IsolationMode.InstancePerLeaf
+}
+
